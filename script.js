1583c3ed13691b60ce5497c824b54c6186fceda5
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-08-01 01:38:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const makeChange = (c) => {
   // your name here
-};
+
+ const change = {
+        q: 0, 
+        d: 0, 
+        n: 0,
+        p: 0  
+    };
+
+    // Define coin values in cents
+    const coinValues = {
+        q: 25, 
+        d: 10, 
+        n: 5,  
+        p: 1   
+    };
+
+    
+    change.q = Math.floor(c / coinValues.q);
+    c %= coinValues.q;
+
+	 change.d= math.floor(c/coinValues.d)
+	c%=coinValues.d
+
+	change.n= math.floor(c/coinValues.n)
+	c%=coinValues.n
+
+	change.p=c
+
+	return change
+}
+
+
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
